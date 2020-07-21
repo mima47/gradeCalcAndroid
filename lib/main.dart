e6a66b7f14f61787b6ca25d30fe22c0a4315e01f
Petruska Martin <martin.petruska@protonmail.com>
2020-07-21 21:44:45 +0200

    added textFields

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:gradecalc/screens/add_user.dart';
 import 'package:gradecalc/screens/login_screen.dart';
 
 void main() => runApp(MaterialApp(
   routes: {
     '/': (context) => LoginScreen(),
-    '/addUser': (context) => AddUser()
+
   },
 ));
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gradecalc/helpers/database_helper.dart';
-import 'package:gradecalc/db_test.dart' as dbTest;
-import 'package:gradecalc/models/user.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -10,19 +7,9 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
 
-  String userValue = 'Martin';
-  List<String> list;
-
-  @override
-  void initState(){
-    // TODO: implement initState
-    super.initState();
-  }
-
-//  void getList() async {
-//    list = await DBProvider.db.getNicknames();
-//    print(list);
-//  }
+  String user;
+  String password;
+  String instCode;
 
   @override
   Widget build(BuildContext context) {
@@ -55,84 +42,67 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
-              SizedBox(height: 60),
-              Text(
-                'Select User',
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Username',
+                  hintStyle: TextStyle(
+                    fontFamily: 'Quicksand',
+                    color: Colors.grey[700]
+                  ),
+                ),
                 style: TextStyle(
                   fontFamily: 'Quicksand',
-                  fontSize: 24,
                   color: Colors.grey[300]
                 ),
-              ),
-              DropdownButton<String>(
-                value: userValue,
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 18,
-                  fontFamily: 'Quicksand'
-                ),
-                onChanged: (String newValue) {
-                  setState(() {
-                    userValue = newValue;
-                  });
+                onChanged: (String str) {
+                  user = str;
                 },
-                items: <String>['Martin']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
               ),
-              SizedBox(height: 25),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: TextStyle(
+                      fontFamily: 'Quicksand',
+                      color: Colors.grey[700]
+                  ),
+                ),
+                style: TextStyle(
+                    fontFamily: 'Quicksand',
+                    color: Colors.grey[300]
+                ),
+                onChanged: (String str){
+                  password = str;
+                },
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Institute Code',
+                  hintStyle: TextStyle(
+                      fontFamily: 'Quicksand',
+                      color: Colors.grey[700]
+                  ),
+                ),
+                style: TextStyle(
+                    fontFamily: 'Quicksand',
+                    color: Colors.grey[300]
+                ),
+                onChanged: (String str){
+                  instCode = str;
+                },
+              ),
               RaisedButton(
-                onPressed: (){},
+                onPressed: () {
+                  print(user);
+                  print(password);
+                  print(instCode);
+                },
                 child: Text(
                   'Login',
                   style: TextStyle(
-                    fontFamily: 'Quicksand',
-                    color: Colors.white,
+                    fontFamily: 'Quicksand'
                   ),
                 ),
-                color: Colors.lightGreen[600],
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: RaisedButton(
-                      onPressed: () async {
-                        await Navigator.pushNamed(context, '/addUser');
-                      },
-                      child: Text(
-                          'Add User',
-                        style: TextStyle(
-                          fontFamily: 'Quicksand',
-                          color: Colors.white
-                        ),
-                      ),
-                      color: Colors.blue,
-                    ),
-                  ),
-                  SizedBox(width: 5),
-                  Expanded(
-                    flex: 1,
-                    child: RaisedButton(
-                      onPressed: () {
-                        dbTest.main();
-//                        DBProvider.db.deleteDB();
-                      },
-                      child: Text(
-                          'Edit User',
-                        style: TextStyle(
-                          fontFamily: 'Quicksand',
-                          color: Colors.white,
-                        ),
-                      ),
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
+                color: Colors.green,
               )
             ],
           ),
@@ -140,9 +110,4 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
-}
-
-Future<void> users() async {
-  dynamic test = await DBProvider.db.getAllPersons();
-  print(await test);
 }
