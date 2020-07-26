32a633c1640280547b4d034942dfa0899485a9c9
Petruska Martin <martin.petruska@protonmail.com>
2020-07-26 11:35:44 +0200

    added default screen

---
+++ lib/screens/default_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:gradecalc/globals.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+
+  Map data = {};
+  String lastMonth;
+  String currentMonth;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    this.data = ModalRoute.of(context).settings.arguments;
+    setState(() {
+      lastMonth = data['lastMonth'];
+      currentMonth = data['currentMonth'];
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      appBar: AppBar(
+        title: Text('Dashboard'),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.attach_money),
+                    title: Text(
+                      'Current Month',
+                      style: TextStyle(
+                        fontFamily: Globals().font
+                      ),
+                    ),
+                  ),
+                  Text(
+                    currentMonth + 'Ft',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 42,
+                      fontFamily: Globals().font
+                    ),
+                  ),
+                  SizedBox(height: 8,)
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.attach_money),
+                    title: Text(
+                      'Last Month',
+                      style: TextStyle(
+                          fontFamily: Globals().font
+                      ),
+                    ),
+                  ),
+                  Text(
+                    lastMonth + 'Ft',
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 42,
+                        fontFamily: Globals().font
+                    ),
+                  ),
+                  SizedBox(height: 8,)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
