ccaa368bcd571caf6c991dbc350bba11b36c5e38
Petruska Martin <martin.petruska@protonmail.com>
2020-07-26 11:34:18 +0200

    added more routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:gradecalc/screens/default_screen.dart';
 import 'package:gradecalc/screens/login_screen.dart';
 import 'package:gradecalc/screens/main_screen.dart';
 import 'package:gradecalc/screens/token_screen.dart';
+import 'package:gradecalc/screens/loading_screen.dart';
 
 void main() => runApp(MaterialApp(
   routes: {
     '/': (context) => LoginScreen(),
-    '/mainScreen': (context) => TestScreen(),
+    '/testScreen': (context) => TestScreen(),
+    '/mainScreen': (context) => MainScreen(),
     '/tokenScreen': (context) => TokenScreen(),
+    '/loadingScreen': (context) => LoadingScreen(),
   },
 ));
