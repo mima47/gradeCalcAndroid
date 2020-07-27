24061937341cc305961bba64efc185a64a932b54
Petruska Martin <martin.petruska@protonmail.com>
2020-07-27 12:55:34 +0200

    refactor (deprecated)

--- lib/screens/token_screen.dart
+++ lib/screens/token_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gradecalc/helpers/request_helper.dart';
 
 class TokenScreen extends StatefulWidget {
   @override
@@ -7,21 +6,17 @@ class TokenScreen extends StatefulWidget {
 }
 
 class _TokenScreenState extends State<TokenScreen> {
-
   Map data = {};
 
   @override
   Widget build(BuildContext context) {
-
     data = ModalRoute.of(context).settings.arguments;
     String accessToken = data['token'];
 
     return Scaffold(
       body: SafeArea(
         child: Column(
-          children: [
-            Text(accessToken)
-          ],
+          children: [Text(accessToken)],
         ),
       ),
     );
