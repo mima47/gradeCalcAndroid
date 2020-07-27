04732db955c9bc280fa33c02f5f3a10c85372a63
Petruska Martin <martin.petruska@protonmail.com>
2020-07-27 23:35:56 +0200

    listView user select

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,7 +3,7 @@ import 'package:gradecalc/models/user.dart';
 import 'package:gradecalc/globals.dart';
 import 'package:gradecalc/helpers/database_helper.dart' as dbhelper;
 import 'package:gradecalc/screens/add_user_screen.dart';
-import 'package:gradecalc/screens/default_screen.dart';
+import 'package:gradecalc/screens/user_list_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -13,7 +13,7 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
 
   Future<List<User>> future;
-  User _selectedUser;
+  User selectedUser = User(nick: '', username: '', password: '', instCode: '');
   String user;
   String password;
   String instCode;
@@ -35,13 +35,24 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   addUserScreen(BuildContext context) async {
-    final result = await Navigator.push(
+    await Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => AddUserScreen())
     );
 
     setState(() {
       future = _getUsers();
+    });
+  }
+
+  selectUserScreen(BuildContext context) async {
+    List list = await _getUsers();
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => UserListScreen(list: list))
+    );
+    setState(() {
+      selectedUser = result;
     });
   }
 
@@ -53,6 +64,9 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    if (selectedUser == null){
+      selectedUser = User(nick: 'No user selected', username: '', password: '', instCode: '');
+    }
     return Scaffold(
       backgroundColor: Colors.grey[800],
       body: SafeArea(
@@ -80,91 +94,75 @@ class _LoginScreenState extends State<LoginScreen> {
                       color: Colors.grey[300]),
                 ),
               ),
-              FutureBuilder <List<User>>(
-                future: future,
-                builder: (BuildContext context, AsyncSnapshot<List<User>> snapshot){
-                  if (!snapshot.hasData){
-                    return CircularProgressIndicator();
-                  }
-                  return Column(
-                    children: [
-                      DropdownButton<User>(
-                        hint: Text('Choose a user'),
-                        value: _selectedUser,
-                        onChanged: (User value) {
+              FlatButton(
+                color: Colors.grey[850],
+                onPressed: (){
+                  selectUserScreen(context);
+                },
+                child: Text(
+                  'Selected user: ${selectedUser.nick}',
+                  style: TextStyle(
+                    color: Colors.grey[300],
+                    fontFamily: Globals().font,
+                  ),
+                ),
+              ),
+              RaisedButton(
+                color: Colors.green,
+                onPressed: (){
+                  Navigator.pushNamed(context, '/loadingScreen', arguments: {
+                    'username': selectedUser.username,
+                    'password': selectedUser.password,
+                    'instCode': selectedUser.instCode
+                  });
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.grey[300],
+                    fontFamily: Globals().font
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: RaisedButton(
+                      onPressed: (){
+                        addUserScreen(context);
+                      },
+                      child: Text(
+                        'Add User',
+                        style: TextStyle(
+                          color: Colors.grey[300],
+                          fontFamily: Globals().font,
+                        ),
+                      ),
+                      color: Colors.blue[600],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                      child: RaisedButton(
+                        onPressed: (){
+                          dbhelper.deleteUser(selectedUser);
                           setState(() {
-                            _selectedUser = value;
+                            selectedUser = User(nick: '', username: '', password: '', instCode: '');
                           });
                         },
-                        isExpanded: false,
-                        items: snapshot.data.map((User user) {
-                          return DropdownMenuItem<User>(
-                            value: user,
-                            child: Text(
-                                user.nick,
-                              style: TextStyle(
-                                fontFamily: Globals().font,
-                                color: Colors.blueAccent
-                              ),
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                      RaisedButton(
-                        onPressed: () {
-                          user = _selectedUser.username;
-                          password = _selectedUser.password;
-                          instCode = _selectedUser.instCode;
-
-                          Navigator.pushReplacementNamed(context, '/loadingScreen',
-                              arguments: {
-                                'username': user,
-                                'password': password,
-                                'instCode': instCode
-                              });
-                        },
                         child: Text(
-                          'Login',
-                          style: TextStyle(fontFamily: 'Quicksand'),
-                        ),
-                        color: Colors.green,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: Container(
-                              margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
-                              child: RaisedButton(
-                                onPressed: (){
-                                  addUserScreen(context);
-                                },
-                                child: Text(
-                                  'Add user',
-                                  style: TextStyle(fontFamily: Globals().font),
-                                ),
-                              ),
-                            ),
+                          'Delete User',
+                          style: TextStyle(
+                            color: Colors.grey[300],
+                            fontFamily: Globals().font,
                           ),
-                          Expanded(
-                            child: RaisedButton(
-                              color: Colors.red,
-                              onPressed: (){
-                                dbhelper.deleteUser(_selectedUser);
-                                // try-catch(?)
-                              },
-                              child: Text(
-                                'Delete user',
-                                style: TextStyle(
-                                    fontFamily: Globals().font
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
+                        ),
+                        color: Colors.red,
                       ),
-                    ],
-                  );
-                },
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
