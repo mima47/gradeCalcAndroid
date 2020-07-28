d9757332535ba03558d146c4e0ddcfe7879c7537
Petruska Martin <martin.petruska@protonmail.com>
2020-07-28 17:56:12 +0200

    current_month_details_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+
+import 'package:gradecalc/screens/current_month_details_screen.dart';
 import 'package:gradecalc/screens/custom_time_screen.dart';
-import 'package:gradecalc/screens/default_screen.dart';
-import 'package:gradecalc/screens/login_screen.dart';
-import 'package:gradecalc/screens/main_screen.dart';
-import 'package:gradecalc/screens/loading_screen.dart';
 import 'package:gradecalc/screens/add_user_screen.dart';
+import 'package:gradecalc/screens/default_screen.dart';
+import 'package:gradecalc/screens/loading_screen.dart';
+import 'package:gradecalc/screens/login_screen.dart';
 
 void main() => runApp(MaterialApp(
   routes: {
     '/': (context) => LoginScreen(),
-    '/testScreen': (context) => TestScreen(),
     '/mainScreen': (context) => MainScreen(),
     '/loadingScreen': (context) => LoadingScreen(),
     '/addUserScreen': (context) => AddUserScreen(),
     '/customTimeScreen': (context) => CustomTimeScreen(),
+    '/currentMonthDetailsScreen': (context) => CurrentMonthDetailsScreen(),
   },
   theme: ThemeData(
     primaryColor: Colors.lightGreen,
