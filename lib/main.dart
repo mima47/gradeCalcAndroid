70f7676aad42a7a33b2853bd5303f1aa434469fa
Petruska Martin <martin.petruska@protonmail.com>
2020-07-30 15:12:58 +0200

    renamed currentMonthDetails

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:gradecalc/screens/current_month_details_screen.dart';
+import 'package:gradecalc/screens/month_details_screen.dart';
 import 'package:gradecalc/screens/custom_time_screen.dart';
 import 'package:gradecalc/screens/add_user_screen.dart';
 import 'package:gradecalc/screens/default_screen.dart';
@@ -14,7 +14,7 @@ void main() => runApp(MaterialApp(
     '/loadingScreen': (context) => LoadingScreen(),
     '/addUserScreen': (context) => AddUserScreen(),
     '/customTimeScreen': (context) => CustomTimeScreen(),
-    '/currentMonthDetailsScreen': (context) => CurrentMonthDetailsScreen(),
+    '/monthDetailsScreen': (context) => MonthDetailsScreen(),
   },
   theme: ThemeData(
     primaryColor: Colors.lightGreen,
